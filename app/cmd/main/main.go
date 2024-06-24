81ea188d9a6c493c381c4f7ef275ebb4719ff45c
Lucas <lucasmonir@hotmail.com>
2024-06-24 13:30:47 -0300

    Dockerized the application, TODO: Migrate and Dockerize the database

---
+++ app/cmd/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"movie-watchlist/pkg/controller"
+	repo "movie-watchlist/pkg/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	if !repo.CheckStorageCreated() {
+		fmt.Println("Error while initializing database!")
+		return
+	}
+
+	startServer()
+}
+
+func startServer() {
+	base := "/movies"
+	router := gin.Default()
+
+	router.GET(base, controller.GetMovies)
+	router.GET(base+"/:id", controller.GetMovie)
+	router.DELETE(base+"/delete/:id", controller.DeleteMovie)
+	router.POST(base+"/insert", controller.AddMovie)
+	router.PATCH(base+"/rate/:id", controller.RateMovie)
+	router.Run("0.0.0.0:9800")
+}
---
+++ app/pkg/repository/movieRepository.go
@@ -0,0 +1,140 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"movie-watchlist/pkg/models"
+	"movie-watchlist/pkg/utils"
+	"os"
+	"slices"
+)
+
+// Temporary file storage... migrate to mysql
+func CheckStorageCreated() bool {
+	jsonPath := utils.GetTaskFilePath()
+
+	if !CheckStorageExists(jsonPath) {
+		initStorage(jsonPath)
+	}
+
+	return true
+}
+
+// Temporary file storage... migrate to mysql
+func CheckStorageExists(jsonPath string) bool {
+	info, err := os.Stat(jsonPath)
+	return !(os.IsNotExist(err) || utils.CheckError(err) || info.IsDir() || utils.IsFileEmpty(jsonPath))
+}
+
+// Temporary file storage... migrate to mysql
+func initStorage(jsonPath string) {
+
+	err := os.WriteFile(jsonPath, []byte("[]"), fs.ModePerm)
+	if utils.CheckError(err) {
+		fmt.Println(err.Error())
+	}
+}
+
+func ReadMovies() ([]models.Movie, error) {
+	var movies []models.Movie
+	jsonPath := utils.GetTaskFilePath()
+
+	CheckStorageCreated()
+
+	jsonFile, err := os.ReadFile(jsonPath)
+
+	if utils.CheckError(err) {
+		return movies, err
+	}
+
+	err = json.Unmarshal(jsonFile, &movies)
+
+	if utils.CheckError(err) {
+		return movies, err
+	}
+
+	return movies, nil
+}
+
+func CreateMovie(name string) (bool, error) {
+	movie, err := models.NewMovie(name)
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error creating new movie, check the parameters")
+	}
+
+	movies, err := ReadMovies()
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error while reading stored movies")
+	}
+
+	if len(movies) != 0 {
+		movie.Id = getLastId(movies)
+	} else {
+		movie.Id = 1
+	}
+
+	movies = append(movies, movie)
+	json, err := json.MarshalIndent(movies, "", "	")
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error Marshaling the JSON")
+	}
+
+	err = writeToStorage(json)
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error writing movie to storage")
+	}
+
+	fmt.Println("Movie added successfully!")
+	return true, nil
+}
+
+func RateMovie(id int, rating float32) (bool, error) {
+	if !checkRating(rating) {
+		return false, fmt.Errorf("invalid Rating: %f", rating)
+	}
+
+	movies, err := ReadMovies()
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error while reading movies")
+	}
+
+	movieIndex := slices.IndexFunc(movies, func(m models.Movie) bool { return m.Id == id })
+	movie := movies[movieIndex]
+
+	fmt.Println(movie)
+
+	movie.Rating = rating
+	movies[movieIndex] = movie
+
+	json, err := json.MarshalIndent(movies, "", "	")
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error serializing to json")
+	}
+
+	err = writeToStorage(json)
+
+	if utils.CheckError(err) {
+		return false, fmt.Errorf("error serializing to json")
+	}
+
+	return true, nil
+}
+
+func getLastId(movies []models.Movie) int {
+	return movies[len(movies)-1].Id + 1
+}
+
+func writeToStorage(movies []byte) error {
+	return os.WriteFile(utils.GetTaskFilePath(), movies, 0644)
+}
+
+func checkRating(rating float32) bool {
+	return rating >= 0 && rating <= 10
+}
